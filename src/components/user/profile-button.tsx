5453ca440e5984e24ea2a6f51110984da656074e
Matias-DR <Matias-DR@outlook.com>
2024-02-07 01:03:52 -0300

    ADD: profile button

---
+++ src/components/user/profile-button.tsx
@@ -0,0 +1,9 @@
+export default function ProfileButtonComponent() {
+  return <div className='relative inline-block size-9'>
+    <button
+      className='size-full bg-blue-600 font-bold rounded-full border-2 border-blue-400 hover:bg-blue-500 hover:border-blue-300 active:bg-blue-700 active:border-0'
+    >
+      M
+    </button>
+  </div>
+}
