4e78f486baed1b1899fbe019bd5792ea473b5098
Matias-DR <Matias-DR@outlook.com>
2024-02-07 00:58:43 -0300

    ADD: sign-in form component

---
+++ src/components/sign/in/form.component.tsx
@@ -0,0 +1,59 @@
+import {
+  AComponent,
+  InputComponent
+} from '@/components'
+import {
+  ButtonType,
+  InputType
+} from '@/commons/element-types'
+
+
+export default function SigninFormComponent() {
+  return <div className='w-fit'>
+    <form
+      className='bg-zinc-800 shadow-md px-8 pt-6 pb-8 mb-4'
+      noValidate
+    >
+      <div className='mb-4'>
+        <InputComponent
+          id='username'
+          label='Nombre de usuario'
+          type={InputType.TEXT}
+          helper=''
+        />
+      </div>
+      <div className='mb-8'>
+        <InputComponent
+          id='password'
+          label='Contraseña'
+          type={InputType.PASSWORD}
+          helper=''
+        />
+      </div>
+      <div className='flex items-center mb-6'>
+        <div className='w-36 h-12 flex justify-center items-center'>
+          <button
+            type={ButtonType.SUBMIT}
+            className='py-2 px-4 font-bold text-zinc-100 bg-blue-600 hover:bg-blue-800 border-b-2 border-e-2 border-blue-400 active:border-0'
+          >
+            Iniciar Sesión
+          </button>
+        </div>
+        <div className='mx-2' />
+        <AComponent
+          href='/sign/password-recuperation'
+          className='text-sm sm:text-base'
+        >
+          Ah olvidado su contraseña?
+        </AComponent>
+      </div>
+      <div className='w-full text-center text-zinc-300'>
+        Aún no se ah registrado? <AComponent
+          href='/sign/up'
+        >
+          Cree una cuenta
+        </AComponent>
+      </div>
+    </form>
+  </div>
+}
