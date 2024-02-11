c5d53e394ca95e99dba5d82b781a90cc68e1b985
Matias-DR <Matias-DR@outlook.com>
2024-02-11 05:46:52 -0300

    ADD: exports

--- src/components/todo/index.ts
+++ src/components/todo/index.ts
@@ -1,2 +1,6 @@
+export {
+  default as CreateTaskComponent,
+  type TaskType
+} from './create-task'
 export { default as TaskComponent } from './task'
 export { default as TaskSetComponent } from './task-set'
