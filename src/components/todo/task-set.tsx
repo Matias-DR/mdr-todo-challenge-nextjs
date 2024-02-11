d58de89b3bcdd9788de166aeaa41726ecf3c3678
Matias-DR <Matias-DR@outlook.com>
2024-02-11 05:47:42 -0300

    MOD: render the create task component, grided the tasks and aggregate logics

--- src/components/todo/task-set.tsx
+++ src/components/todo/task-set.tsx
@@ -1,48 +1,107 @@
 import { HomeContext } from '@/contexts'
-import { TaskComponent } from './'
-import { useContext, useEffect, useState } from 'react'
+import {
+  CreateTaskComponent,
+  TaskType,
+  TaskComponent
+} from './'
+import {
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
 
 
-const _tasks = [
-  { title: '1', description: '1' },
-  { title: '2', description: '2' },
-  { title: '3', description: '3' },
-  { title: '4', description: '4' },
-  { title: '5', description: '5' }
-]
+/**
+ * A task set component that can be used to display a set of tasks and
+ * aggregate them.
+ */
+export default function TaskSetComponent(): ReactNode {
+  const [tasks, setTasks] = useState<any[]>([])
+  const {
+    status,
+    searched,
+    dateFrom,
+    dateTo
+  } = useContext(HomeContext)
 
-export default function TaskSetComponent() {
-  const [tasks, setTasks] = useState(_tasks)
-  const context = useContext(HomeContext)
+  /**
+   * Create a task, aggregate it to the set of tasks and send it to the
+   * endpoint for creating tasks. Does not allow to create without both title
+   * and description, needs almost one of them.
+   * @param {TaskType} task - The task to create.
+   */
+  const handleCreate = (task: TaskType) => {
+    if (!task.title && !task.description) return
+    // POST /api/task/ → setTasks(tasksFetched)
+    setTasks([
+      task,
+      ...tasks
+    ])
+  }
 
-  useEffect(() => { }, [tasks])
+  // const handleModify = ()
 
+  /**
+   * Remove a task by primary key and send it to the endpoint for removing
+   * tasks.
+   * @param pk - The primary key of the task to remove.
+   */
+  const handleRemove = (pk: number): void => {
+    setTasks(tasks.filter((_, index) => index !== pk))
+  }
+
+  /**
+   * Create the order of the task by your index.
+   * @param index - The index of the task.
+   */
+  const craftOrder = (index: number): string => `order-${index + 1}`
+
+  // Re-render the list of tasks when they are removed or added.
   useEffect(() => {
-    if (context.add) {
-      context.setAdd(false)
-      setTasks([
-        { title: '', description: '' },
-        ...tasks
-      ])
-    }
-  },
-    [context.add]
-  )
+    console.log('tasks', tasks)
+  }, [tasks])
 
-  return <div className='w-full flex flex-wrap place-content-start gap-2'>
-    {Object.entries(tasks).map((
-      entry,
+  return <div className='
+    w-full
+    grid
+    grid-rows-2
+    grid-cols-1 sm:grid-cols-3 2xl:grid-cols-4
+    gap-2
+  '>
+
+    {/* Add task button */}
+    <div
+      key={'add-task-button'}
+      className='
+        min-h-[13.1rem]
+        row-span-2
+        order-0
+      '
+    >
+      <CreateTaskComponent handleCreate={handleCreate} />
+    </div>
+
+    {/* Tasks */}
+    {tasks.map((
+      elem,
       index
     ) => <div
-      key={'TaskComponent-container-' + entry[0]}
-      className={`basis-full sm:basis-[49.41%] lg:basis-[32.9%] 2xl:basis-[24.6%] ${index === 0 ? 'order-first' : ''}`}
+      key={'task-container-' + index}
+      className={`
+        size-full
+        ${craftOrder(index)}
+      `}
     >
-        <TaskComponent
-          key={'TaskComponent-' + entry[0]}
-          id={entry[0]}
-          title={entry[1].title}
-          description={entry[1].description}
-        />
-      </div>)}
+      <TaskComponent
+        key={'task-' + index}
+        pk={index}
+        title={elem.title}
+        description={elem.description}
+        completed={false}
+        created={new Date()}
+        handleRemove={handleRemove}
+      />
+    </div>)}
   </div>
 }
