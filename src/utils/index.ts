4e1bd738fc65ef305f6b46c002dceed1e2912bc8
Matias-DR <Matias-DR@outlook.com>
2024-02-13 19:13:42 -0300

    MOD: exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as serverSideSignedVerify } from './server-side-signed-verify'
 export { default as serverSideUnsignedVerify } from './server-side-unsigned-verify'
+export { default as extractUserFromServerContext } from './extract-user-from-server-context'
