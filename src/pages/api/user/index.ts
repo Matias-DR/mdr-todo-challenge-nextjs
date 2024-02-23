0eeade3b8ef121f0419cc84224bfa1e5bc6c00ca
Matias-DR <Matias-DR@outlook.com>
2024-02-23 00:05:13 -0300

    MOD: user upd working

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,42 +1,62 @@
 import axios from 'axios'
 
-import type {
-  NextApiRequest,
-  NextApiResponse
-} from 'next'
-import { UpdateFormData } from '@/components/user'
-
+import { decode } from 'jsonwebtoken'
+import { type UpdateFormData } from '@/components/user'
+import { extractAccessFromRequest } from '@/utils'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 /**
  * Async handler function that sends the update form data to the external server.
  * @arg {NextApiRequest} req
  * @arg {NextApiResponse} res
  */
-export default async function handler(
+export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const formData: UpdateFormData = req.body
-  const url = process.env.API_HOST + `user/${7}/`
+  const formData = req.body as UpdateFormData
+  const access = extractAccessFromRequest(req)
+  const { pk, email, username } = decode(access) as { pk: string, email: string, username: string }
+  const url = process.env.API_HOST + `user/${pk}/`
 
-  await axios.patch(url, formData)
+  if (formData.email === '') {
+    formData.email = email
+  }
 
-    .then((response: any) => {
+  const data = {
+    ...formData,
+    username
+  }
+
+  console.log(pk, data)
+
+  await axios
+    .patch(url, data, { headers: { Authorization: `Bearer ${access}` } })
+
+    .then((response: { status: number }) => {
       res
         .status(response.status)
         .json({ message: 'Usuario actualizado exitosamente.' })
     })
 
-    .catch((error: any) => {
-      let message = ''
-      try {
-        Object.entries(error.response.data).map((entry: any) => {
-          const entrySlice = entry.slice(1)
-          message = message.concat(entrySlice.join('. '))
-        })
-      } catch {
-        message = 'Ah ocurrido un error inesperado, por favor intente nuevamente.'
+    .catch(
+      (error: {
+        response: { data: Record<string, string[]>, status: number }
+      }) => {
+        let message = ''
+        try {
+          Object.entries(error.response.data).map(
+            (entry: [string, unknown]): void => {
+              const entrySlice = entry.slice(1)
+              message = message.concat(entrySlice.join('. '))
+              return undefined
+            }
+          )
+        } catch {
+          message =
+            'Ah ocurrido un error inesperado, por favor intente nuevamente.'
+        }
+        res.status(error.response.status).json({ message })
       }
-      res.status(error.response.status).json({ message })
-    })
+    )
 }
