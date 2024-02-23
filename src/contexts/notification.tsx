88338c1b4eb52e682303d7fdd4b3c964512a5d5c
Matias-DR <Matias-DR@outlook.com>
2024-02-23 00:09:13 -0300

    DEL: removed unused vars & MOD: document formated & simplified setting notification

--- src/contexts/notification.tsx
+++ src/contexts/notification.tsx
@@ -1,77 +1,54 @@
-import { NotificationComponent } from '@/components'
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useState
-} from 'react'
+import { createContext, type ReactNode, useContext, useState } from 'react'
 
-export enum NotificationStatus {
-  SUCCESS = 'success',
-  ERROR = 'error',
-  WARNING = 'warning',
-  INFO = 'info'
-}
-
-type Theme = Record<
-  NotificationStatus,
-  {
-    div: string
-    p: string
-    shape: string
-  }
->
-
-const theme: Theme = {
-  success: {
-    div: 'border-green-500',
-    p: 'text-green-500',
-    shape: 'border-l-green-500'
-  },
-  error: {
-    div: 'border-red-500',
-    p: 'text-red-500',
-    shape: 'border-l-red-500'
-  },
-  warning: {
-    div: 'border-orange-500',
-    p: 'text-orange-500',
-    shape: 'border-l-orange-500'
-  },
-  info: {
-    div: 'border-blue-500',
-    p: 'text-blue-500',
-    shape: 'border-l-blue-500'
-  }
-}
+export type NotificationStatus = 'success' | 'error' | 'warning' | 'info'
 
 const NotificationContext = createContext({
   message: '',
-  setMessage: (value: string) => { },
-  status: NotificationStatus.INFO,
-  setStatus: (value: NotificationStatus) => { }
+  setMessage: (value: string) => {},
+  status: 'info' as NotificationStatus,
+  setStatus: (value: NotificationStatus) => {},
+  setNotification: (message: string, status: NotificationStatus) => {}
 })
 
 interface Props {
   children: ReactNode
 }
 
-export default function NotificationContextProvider({ children }: Props) {
+export default function NotificationContextProvider ({
+  children
+}: Props): React.ReactNode {
   const [message, setMessage] = useState<string>('')
-  const [status, setStatus] = useState<NotificationStatus>(
-    NotificationStatus.INFO
-  )
+  const [status, setStatus] = useState<NotificationStatus>('info')
 
-  return <NotificationContext.Provider value={{
-    message,
-    setMessage,
-    status,
-    setStatus
-  }}>
-    {children}
-  </NotificationContext.Provider>
+  const setNotification = (
+    message: string,
+    status: NotificationStatus
+  ): void => {
+    setMessage(message)
+    setStatus(status)
+  }
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        message,
+        setMessage,
+        status,
+        setStatus,
+        setNotification
+      }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  )
 }
 
-export const useNotificationContext = () => {
+export const useNotificationContext = (): {
+  message: string
+  setMessage: (value: string) => void
+  status: NotificationStatus
+  setStatus: (value: NotificationStatus) => void
+  setNotification: (message: string, status: NotificationStatus) => void
+} => {
   return useContext(NotificationContext)
 }
