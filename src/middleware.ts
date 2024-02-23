1ce47863d987f94d41fededcd0683a972a3fd7b1
Matias-DR <Matias-DR@outlook.com>
2024-02-23 00:09:39 -0300

    DEV: middleware for token treatment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,68 +1,45 @@
-import axios from 'axios'
-
-import {
-  NextResponse,
-  type NextRequest
-} from 'next/server'
+import { type NextApiRequest, type NextApiResponse } from 'next'
+import { NextResponse } from 'next/server'
 import { setTokensInServerContext } from '@/utils'
 
-
-const exclude = [
-  '/api/sign/in',
-  '/sign/in',
-  '/api/sign/up',
-  '/sign/up',
-  '/rights',
-  '/sign/password-recuperation',
-  '/_next/static',
-  '/_next/image',
-  '/favicon.ico'
-]
-
-const url = process.env.API_HOST + `token/refresh/`
-
+const URL = process.env.API_HOST + 'token/refresh/'
 /**
  * Middleware function that refreshes the access token if it has expired before
  * calling the endpoint handler
  */
-export default async function refresh(
-  req: NextRequest
-) {
-  // If the endpoint is not in the exclude list
-  if (!exclude.some((path) => req.nextUrl.pathname.startsWith(path))) {
-
-    // Get the access and refresh tokens from the cookies
-    const access = req.cookies.get('access')
-    const refresh = req.cookies.get('refresh')
-
-    // If access has caduced
-    if (access === undefined || access === null) {
-      // And refresh has not caduced, then refresh access
-      if (refresh !== undefined && refresh !== null) {
-        try {
-          const result = await axios.post(
-            url,
-            { refresh }
-          )
-          setTokensInServerContext(
-            refresh.value,
-            result.data.access,
-            undefined,
-            req
-          )
-        } catch {
-          // If refresh has caduced or another error occurs, redirect to the
-          // login page
-          return NextResponse.redirect('/')
-        }
-      } else {
-        // If refresh has caduced, redirect to the login page
-        return NextResponse.redirect('/')
-      }
-    }
+export default function refresh (req: NextApiRequest, res: NextApiResponse): void {
+  let access: string
+  let refresh: string
+  try {
+    access = String(req.cookies).split('; ')[0].split('=')[1]
+  } catch {
+    access = ''
   }
-
+  try {
+    refresh = String(req.cookies).split('; ')[1].split('=')[1]
+  } catch {
+    refresh = ''
+  }
+  if ((access.length === 0) && (refresh.length > 0)) {
+    fetch(URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ refresh })
+    })
+      .then(async (response: { json: () => Promise<{ access: string }> }) => {
+        const { access } = await response.json()
+        setTokensInServerContext(refresh, access, res)
+      })
+      .catch((error: unknown) => {
+        // Habría que "signout" y redirigir a la página de inicio de sesión
+        console.error(error)
+      })
+  }
   // If the endpoint is included in the exclude list or the access token has
   // not expired, continue with the request
   NextResponse.next()
+}
+
+export const config = {
+  matcher: '/((?!api/sign/in|sign/in|api/sign/up|sign/up|api/sign/password_recuperation|api/rights|_next/static|_next/image|favicon.ico).*)'
 }
