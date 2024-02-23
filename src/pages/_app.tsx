731742cf92c96917914e24dadddcfbafc3deca9b
Matias-DR <Matias-DR@outlook.com>
2024-02-23 00:02:56 -0300

    MOD: prettier formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { NotificationContextProvider } from '@/contexts'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({
+export default function App ({
   Component,
   pageProps
-}: AppProps) {
-  return <NotificationContextProvider>
-    <Component {...pageProps} />
-  </NotificationContextProvider>
+}: AppProps): React.ReactNode {
+  return (
+    <NotificationContextProvider>
+      <Component {...pageProps} />
+    </NotificationContextProvider>
+  )
 }
