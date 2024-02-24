973c0e8817177f796c5bd823d943d70421c05ef6
Matias-DR <Matias-DR@outlook.com>
2024-02-24 17:08:02 -0300

    MOD: cookies managed by cookies-next

--- src/pages/api/sign/in.ts
+++ src/pages/api/sign/in.ts
@@ -1,8 +1,9 @@
-import axios from 'axios'
+import type { SignupFormData } from '@/components/sign/up/form';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { setTokensInServerContext } from '@/utils'
-import { type SignupFormData } from '@/components/sign/up/form'
+import { setToken } from '@/utils';
+
+import axios from 'axios';
 
 /**
  * Async handler function that sends the signin form data to the external server.
@@ -10,46 +11,39 @@ import { type SignupFormData } from '@/components/sign/up/form'
  * @arg {NextApiRequest} req
  * @arg {NextApiResponse} res
  */
-export default async function handler (
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ): Promise<void> {
-  const formData = req.body as SignupFormData
-  const url = process.env.API_HOST + 'token/'
+  const formData = req.body as SignupFormData;
+  const url = process.env.BACK_HOST + '/api/token/';
 
   await axios
     .post(url, formData)
-
     .then(
-      (response: {
-        data: { access: string, refresh: string }
-        status: number
-      }) => {
-        const { access, refresh } = response.data
-        setTokensInServerContext(refresh, access, res)
-        res.status(response.status).json({})
-      }
+      (response: { data: { access: string; refresh: string }; status: number }) => {
+        const { access, refresh } = response.data;
+        setToken('refresh', refresh, req, res);
+        setToken('access', access, req, res, new Date(new Date().setSeconds(new Date().getSeconds() + 3)));
+        res.status(response.status).json({});
+      },
     )
-
     .catch(
-      (error: {
-        response: { data: Record<string, string[]>, status: number }
-      }) => {
-        let message = ''
+      (error: { response: { data: Record<string, string[]>; status: number } }) => {
+        let message = '';
         try {
           Object.entries(error.response.data).map(
             (entry: [string, unknown]): void => {
-              const entrySlice = entry.slice(1)
-              message = message.concat(entrySlice.join('. '))
-              return undefined
-            }
-          )
-          res.status(error.response.status).json({ message })
+              const entrySlice = entry.slice(1);
+              message = message.concat(entrySlice.join('. '));
+              return undefined;
+            },
+          );
+          res.status(error.response.status).json({ message });
         } catch {
-          message =
-            'Ah ocurrido un error inesperado, por favor intente nuevamente.'
-          res.status(500).json({ message })
+          message = 'Ah ocurrido un error inesperado, por favor intente nuevamente.';
+          res.status(500).json({ message });
         }
-      }
-    )
+      },
+    );
 }
--- src/pages/api/sign/out.ts
+++ src/pages/api/sign/out.ts
@@ -1,23 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { serialize } from 'cookie'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { deleteCookie } from 'cookies-next';
 
 /**
  * Async handler function that removes the cookie from the response
  * @arg {NextApiRequest} req
  * @arg {NextApiResponse} res
  */
-export default function handler (
-  req: NextApiRequest,
-  res: NextApiResponse
-): void {
-  const access = serialize('access', '', {
-    secure: process.env.NODE_ENV === 'production',
-    path: '/'
-  })
-  const refresh = serialize('refresh', '', {
-    secure: process.env.NODE_ENV === 'production',
-    path: '/'
-  })
-  res.setHeader('Set-Cookie', [access, refresh])
-  res.status(200).json({})
+export default function handler(req: NextApiRequest, res: NextApiResponse): void {
+  deleteCookie('refresh', { req, res });
+  deleteCookie('access', { req, res });
+  res.status(200).json({});
 }
